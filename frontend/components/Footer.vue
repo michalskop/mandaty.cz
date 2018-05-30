17e4cb1d74d1da4f6ab1145e953eba06e52b9fc5
Michal Skop <michal.skop@kohovolit.eu>
2018-05-30 06:18:54 +0200

    darujme

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="navbar navbar-light bg-light">
+        <div class="container d-flex justify-content-between">
+            <span>Od autorů <a href="https://volebnikalkulacka.cz" class="m-1">Volební kalkulačky</a></span>
+            <span><a href="https://projects.kohovolit.eu">Další projekty autorů</a> • Kontakt: <a href="http://kohovolit.eu/kontakt">KohoVolit.eu</a> • <a href="https://github.com/michalskop/mandaty.cz"><i class="fa fa-github"></i> Kód</a>
+            </span>
+        </div>
+    </footer>
+</template>
+<script>
+</script>
--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,5 @@
 API_URL = "http://127.0.0.1:8001/"
 EXCLUDE_ELECTIONS = "volby"
 LOCALE = "cs"
+API_DARUJME = "https://projects.kohovolit.eu/api/?projectId=1200738"
+URL_DARUJME = "https://www.darujme.cz/projekt/1200738"
