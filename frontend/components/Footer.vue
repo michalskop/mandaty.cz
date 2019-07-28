8ab5142b06cc58e3060eca4db4f5a884ea55a0f7
Michal Skop <michal.skop@kohovolit.eu>
2019-07-28 17:31:38 +0200

    gitignore updated dist

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="navbar navbar-light bg-light">
         <div class="container d-flex justify-content-between">
             <span>Od autorů <a href="https://volebnikalkulacka.cz" class="m-1">Volební kalkulačky</a></span>
-            <span><a href="https://projects.kohovolit.eu">Další projekty autorů</a> • Kontakt: <a href="http://kohovolit.eu/kontakt">KohoVolit.eu</a> • <a href="https://github.com/michalskop/mandaty.cz"><i class="fa fa-github"></i> Kód</a> • <a href="https://api.mandaty.cz"><i class="fa fa-database"></i> data/API</a>
+            <span><a href="https://projects.kohovolit.eu">Další projekty autorů</a> • Kontakt: <a href="http://kohovolit.eu/kontakt">KohoVolit.eu</a> • <a href="https://api.mandaty.cz"><i class="fa fa-database"></i> data/API</a>
             </span>
         </div>
     </footer>
